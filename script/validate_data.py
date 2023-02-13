69333ae45080a16b13c972a01298bdca75c0a988
Neo Liu <liuhuahui88@gmail.com>
2023-02-13 13:31:12 +0800

    Implements Script to Validate Data

---
+++ script/validate_data.py
@@ -0,0 +1,20 @@
+from prophet.data_storage import *
+from prophet.data_validator import *
+
+
+if __name__ == '__main__':
+    validator = StockDataValidator('2010-01-01')
+
+    name_file_path = '../data/chinese_stock_codes.csv'
+    history_dir_path = '../data/history'
+
+    storage = StockDataStorage(name_file_path, history_dir_path)
+
+    for code in storage.get_codes():
+        name = storage.get_name(code)
+        history = storage.load_history(code)
+        summary = validator.validate(code, history)
+        for description in summary:
+            invalid_results = summary[description]
+            for r in invalid_results:
+                print([code, name], description, r)
