0008deccc83435f08aa1a5430855ac8e2590cdb5
Neo Liu <liuhuahui88@gmail.com>
2023-03-01 10:53:06 +0800

    Upgrades Evaluator for Quantile & Win/Tie/Lose Percentage

--- prophet/utils/evaluator.py
+++ prophet/utils/evaluator.py
@@ -19,18 +19,26 @@ class Evaluator:
         return np.exp(np.log(self.get_gain_cum()) / (len(self.values) - 1))
 
     def get_gain_std(self):
-        log_gains = []
-        for i in range(len(self.values) - 1):
-            gain = self.__calculate_gain(self.values[i], self.values[i + 1])
-            log_gains.append(np.log(gain))
+        log_gains = self.__get_log_gains()
         return np.exp(np.std(log_gains))
 
     def get_gain_skew(self):
-        log_gains = []
-        for i in range(len(self.values) - 1):
-            gain = self.__calculate_gain(self.values[i], self.values[i + 1])
-            log_gains.append(np.log(gain))
+        log_gains = self.__get_log_gains()
         return np.exp(sp.stats.skew(log_gains))
+
+    def get_quantile(self, qs):
+        log_gains = self.__get_log_gains()
+        return np.exp([np.quantile(log_gains, q) for q in qs])
+
+    def get_wtl(self):
+        log_gains = self.__get_log_gains()
+
+        num_win = sum(x > 0 for x in log_gains)
+        num_tie = sum(x == 0 for x in log_gains)
+        num_lose = sum(x < 0 for x in log_gains)
+
+        num = num_win + num_tie + num_lose
+        return [num_win / num, num_tie / num, num_lose / num]
 
     def get_sharp_ratio(self):
         return np.exp(np.log(self.get_gain_avg()) / np.log(self.get_gain_std()))
@@ -48,14 +56,26 @@ class Evaluator:
         return worst_drawdown
 
     def __str__(self):
-        return 'gain_cum={:.4f}, gain_avg={:.4f}, gain_std={:.4f}, gain_skew={:.4f},' \
-               ' sharp_ratio={:.4f}, worst_drawdown={:.4f}'.format(
+        return 'cum={:.4f}, avg={:.4f}, std={:.4f}, skew={:.4f},' \
+               ' quantile={}, wtl={}, sharp={:.4f}, drawdown={:.4f}'.format(
             self.get_gain_cum(),
             self.get_gain_avg(),
             self.get_gain_std(),
             self.get_gain_skew(),
+            str(['{:.4f}'.format(n) for n in self.get_quantile([0, 0.1, 0.5, 0.9, 1])]).replace("'", ""),
+            str(['{:.4f}'.format(n) for n in self.get_wtl()]).replace("'", ""),
             self.get_sharp_ratio(),
             self.get_worst_drawdown())
+
+    def __get_gains(self):
+        gains = []
+        for i in range(len(self.values) - 1):
+            gain = self.__calculate_gain(self.values[i], self.values[i + 1])
+            gains.append(gain)
+        return gains
+
+    def __get_log_gains(self):
+        return np.log(self.__get_gains())
 
     @staticmethod
     def __calculate_gain(value1, value2):
