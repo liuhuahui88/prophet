6c15fc078ac7143561c7914492478ae9f51b31cd
Neo Liu <liuhuahui88@gmail.com>
2023-03-07 10:46:25 +0800

    Adds Statistic Features Mean/Std/Skew

--- prophet/agent/imitative_agent.py
+++ prophet/agent/imitative_agent.py
@@ -2,6 +2,7 @@ import collections
 
 import pandas as pd
 import tensorflow as tf
+import scipy as sp
 
 from prophet.agent.abstract_agent import Agent
 from prophet.utils.constant import Const
@@ -58,7 +59,28 @@ class ImitativeAgent(Agent):
     @staticmethod
     def transform_features(features: pd.DataFrame):
         position = features.pop('Position')
-        return {'position': position, 'price': features}
+
+        price = features
+
+        mean = pd.DataFrame()
+        mean['ma5'] = price[['Close-{}'.format(i) for i in range(5)]].mean(axis=1)
+        mean['ma10'] = price[['Close-{}'.format(i) for i in range(10)]].mean(axis=1)
+        mean['ma20'] = price[['Close-{}'.format(i) for i in range(20)]].mean(axis=1)
+        mean['ma30'] = price[['Close-{}'.format(i) for i in range(30)]].mean(axis=1)
+
+        std = pd.DataFrame()
+        std['std5'] = price[['Close-{}'.format(i) for i in range(5)]].std(axis=1)
+        std['std10'] = price[['Close-{}'.format(i) for i in range(10)]].std(axis=1)
+        std['std20'] = price[['Close-{}'.format(i) for i in range(20)]].std(axis=1)
+        std['std30'] = price[['Close-{}'.format(i) for i in range(30)]].std(axis=1)
+
+        skew = pd.DataFrame()
+        skew['skew5'] = sp.stats.skew(price[['Close-{}'.format(i) for i in range(5)]], axis=1)
+        skew['skew10'] = sp.stats.skew(price[['Close-{}'.format(i) for i in range(10)]], axis=1)
+        skew['skew20'] = sp.stats.skew(price[['Close-{}'.format(i) for i in range(20)]], axis=1)
+        skew['skew30'] = sp.stats.skew(price[['Close-{}'.format(i) for i in range(30)]], axis=1)
+
+        return {'position': position, 'price': price, 'mean': mean, 'std': std, 'skew': skew}
 
     @staticmethod
     def extract_samples(history: pd.DataFrame, actions, window_size):
@@ -112,9 +134,12 @@ class ImitativeAgent(Agent):
 
     @staticmethod
     def create_model():
-        input1 = tf.keras.layers.Input(name='position', shape=(1,))
-        input2 = tf.keras.layers.Input(name='price', shape=(30,))
-        inputs = [input1, input2]
+        input1 = tf.keras.layers.Input(name='price', shape=(30,))
+        input2 = tf.keras.layers.Input(name='position', shape=(1,))
+        input3 = tf.keras.layers.Input(name='mean', shape=(4,))
+        input4 = tf.keras.layers.Input(name='std', shape=(4,))
+        input5 = tf.keras.layers.Input(name='skew', shape=(4,))
+        inputs = [input1, input2, input3, input4, input5]
 
         x = tf.keras.layers.Concatenate()(inputs)
         x = tf.keras.layers.Dense(128, activation='relu')(x)
