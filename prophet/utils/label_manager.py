8729397f82ac5b5ad497e72ed178886780543e62
Neo Liu <liuhuahui88@gmail.com>
2023-03-10 02:49:56 +0800

    Implements NextDirection & NextLogGain in LabelManager

--- prophet/utils/label_manager.py
+++ prophet/utils/label_manager.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 from prophet.utils.constant import Const
 from prophet.utils.graph import Graph
@@ -16,15 +17,24 @@ class LabelManager:
         action = action[window_size - 1:]
         action = action.reset_index(drop=True)
 
-        ctx = {'action': action}
+        price = pd.DataFrame()
+        for i in range(window_size):
+            price['Close-{}'.format(i)] = history.shift(-i)['Close']
+        price = price[window_size - 1:]
+        price = price.reset_index(drop=True)
+
+        ctx = {'action': action, 'price': price}
         return self.graph.compute(self.names, ctx)
 
     @staticmethod
     def create_default_graph():
         graph = Graph()
         graph.register('action')
+        graph.register('price')
         graph.register('action_when_empty', LabelManager.FillNaN(Const.ASK), ['action'])
         graph.register('action_when_full', LabelManager.FillNaN(Const.BID), ['action'])
+        graph.register('next_direction', LabelManager.NextDirection(), ['price'])
+        graph.register('next_log_gain', LabelManager.NextLogGain(), ['price'])
         return graph
 
     class FillNaN(Graph.Function):
@@ -34,3 +44,20 @@ class LabelManager:
 
         def compute(self, inputs):
             return inputs[0].copy().fillna(value=self.default_value)
+
+    class NextDirection(Graph.Function):
+
+        def compute(self, inputs):
+            price = inputs[0]
+            next_direction = pd.DataFrame()
+            next_direction['NextDirection'] = price['Close-1'] > price['Close-0']
+            next_direction['NextDirection'].apply(lambda x: 1 if x else 0)
+            return next_direction
+
+    class NextLogGain(Graph.Function):
+
+        def compute(self, inputs):
+            price = inputs[0]
+            next_log_gain = pd.DataFrame()
+            next_log_gain['NextLogGain'] = np.log((price['Close-1'] / price['Close-0']).fillna(value=1))
+            return next_log_gain
