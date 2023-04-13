95dfbc50e959cf3c901f275a2c0d1744c1add4a2
Neo Liu <liuhuahui88@gmail.com>
2023-04-13 21:08:17 +0800

    Implements Clip & Indicator Function

--- prophet/fg/function/math.py
+++ prophet/fg/function/math.py
@@ -4,6 +4,16 @@ import pandas as pd
 from prophet.utils.graph import Graph
 
 
+class Clip(Graph.Function):
+
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def compute(self, inputs):
+        return np.clip(inputs[0], self.min_value, self.max_value)
+
+
 class Log(Graph.Function):
 
     def compute(self, inputs):
@@ -26,6 +36,24 @@ class Negative(Graph.Function):
 
     def compute(self, inputs):
         return (1 - np.sign(inputs[0])) / 2
+
+
+class Indicator(Graph.Function):
+    def __init__(self, cond):
+        self.cond = cond
+
+    def compute(self, inputs):
+        line = inputs[0].iloc[:, 0].tolist()
+
+        result = []
+        for i in range(len(line)):
+            if self.cond(line[i]):
+                result.append(1)
+            else:
+                result.append(0)
+
+        df = pd.DataFrame({'Indic': result})
+        return df
 
 
 class Add(Graph.Function):
