a00ce7db0d8ce78c0acfc1ac432c96efa073c99a
Neo Liu <liuhuahui88@gmail.com>
2023-04-18 17:23:24 +0800

    Adds SOTA NextLogGain Ranking Training Script

---
+++ script/train_data_predictor_sota_rank.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+
+from prophet.utils.metric import Metric
+from prophet.utils.play_ground import PlayGround
+
+if __name__ == '__main__':
+
+    play_ground = PlayGround(
+        name_file_path='../data/chinese_stock_codes.csv',
+        history_file_path='../data/history',
+        commission_rate=0.0)
+
+    inputs = [
+        # tf.keras.layers.Input(name='log_gain', shape=(1,)),
+        tf.keras.layers.Input(name='log_gain_mean', shape=(1,)),
+        tf.keras.layers.Input(name='log_gain_std', shape=(1,)),
+        tf.keras.layers.Input(name='log_gain_skew', shape=(1,)),
+        tf.keras.layers.Input(name='log_gain_kurt', shape=(1,)),
+
+        tf.keras.layers.Input(name='log_price_rank', shape=(1,)),
+        tf.keras.layers.Input(name='log_price_rank_mean', shape=(1,)),
+        tf.keras.layers.Input(name='log_price_rank_std', shape=(1,)),
+        tf.keras.layers.Input(name='log_price_rank_skew', shape=(1,)),
+        tf.keras.layers.Input(name='log_price_rank_kurt', shape=(1,)),
+
+        tf.keras.layers.Input(name='log_price_rank_diff', shape=(1,)),
+        tf.keras.layers.Input(name='log_price_rank_diff_mean', shape=(1,)),
+        tf.keras.layers.Input(name='log_price_rank_diff_std', shape=(1,)),
+        tf.keras.layers.Input(name='log_price_rank_diff_skew', shape=(1,)),
+        tf.keras.layers.Input(name='log_price_rank_diff_kurt', shape=(1,)),
+
+        tf.keras.layers.Input(name='log_price_rank_diff_diff', shape=(1,)),
+        tf.keras.layers.Input(name='log_price_rank_diff_diff_mean', shape=(1,)),
+        tf.keras.layers.Input(name='log_price_rank_diff_diff_std', shape=(1,)),
+        tf.keras.layers.Input(name='log_price_rank_diff_diff_skew', shape=(1,)),
+        tf.keras.layers.Input(name='log_price_rank_diff_diff_kurt', shape=(1,)),
+
+        tf.keras.layers.Input(name='keep_lose', shape=(1,)),
+        tf.keras.layers.Input(name='keep_gain', shape=(1,)),
+    ]
+
+    t_inputs = []
+    for inpt in inputs:
+        inpt = tf.keras.layers.Dense(16, activation='relu')(inpt)
+        inpt = tf.keras.layers.Dense(16, activation='relu')(inpt)
+        t_inputs.append(inpt)
+
+    x = tf.keras.layers.Concatenate()(inputs + t_inputs)
+    x = tf.keras.layers.Dense(256, activation='relu')(x)
+    x = tf.keras.layers.Dense(256, activation='relu')(x)
+    x = tf.keras.layers.Dense(1, activation='linear', kernel_initializer='zeros', name='next_log_gain')(x)
+
+    model = tf.keras.models.Model(inputs=inputs, outputs=x)
+
+    model.compile(optimizer='adam', loss=Metric.rank, metrics=[Metric.me, Metric.rank])
+
+    symbols = play_ground.storage.get_symbols(lambda s: s[0] == '3' and s <= '300800')
+
+    predictor = play_ground.train_predictor(
+        symbols, '2010-01-01', '2021-01-01', '2022-01-01',
+        model, 1000, 1000, 'val_loss', 3, verbose=True)
+
+    predictor.save('models/temp_rank')
