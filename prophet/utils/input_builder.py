52c51bcdd8cd36322dad6674282ca0c4e061f0b4
Neo Liu <liuhuahui88@gmail.com>
2023-05-07 01:37:58 +0800

    Supports Customized Default Shape in Input Builder

--- prophet/utils/input_builder.py
+++ prophet/utils/input_builder.py
@@ -3,12 +3,13 @@ import tensorflow as tf
 
 class InputBuilder:
 
-    def __init__(self):
+    def __init__(self, default_shape=(1,)):
+        self.default_shape = default_shape
         self.input_name_and_shape = {}
 
     def append(self, name, shape=None):
         assert name not in self.input_name_and_shape
-        self.input_name_and_shape[name] = shape if shape is not None else (1,)
+        self.input_name_and_shape[name] = shape if shape is not None else self.default_shape
         return self
 
     def append_all(self, prefix, suffixes, shape=None):
